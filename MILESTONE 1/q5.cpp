c929dd43d86bdcd9673f83b245820294a0ccf55e
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-03-15 16:56:43 +0530

    q5 submitted.

---
+++ MILESTONE 1/q5.cpp
@@ -0,0 +1,10 @@
+/*Write a program to find multiplication of two numbers?*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter two numbers:";
+    cin>>a>>b;
+    int product=a*b;
+    cout<<a<<"*"<<b<<"="<<product;
+}
