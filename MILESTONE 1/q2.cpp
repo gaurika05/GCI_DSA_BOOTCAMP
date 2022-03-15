22208c5ba9a1eaf23628c49cd547adbaa528b62a
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-03-15 16:44:48 +0530

    Add files via upload

---
+++ MILESTONE 1/q2.cpp
@@ -0,0 +1,19 @@
+/*Write a program to print a maximum of two numbers?*/
+#include<iostream>
+using namespace std;
+int main(){
+    int N1,N2;
+    cout<<"enter two numbers:";
+    cin>>N1>>N2;
+    if (N1>N2){
+        cout<<"Maximum="<<N1;
+    }
+    else if(N2>N1){
+            cout<<"Maximum="<<N2;
+    }
+    else if(N1=N2){
+        cout<<"Both are equal:"<<N1;
+    }
+
+
+}
