aac5c64f6725c6a1bf70a09cce2e048f3cb07856
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-03-15 16:56:06 +0530

    q4 submitted.

---
+++ MILESTONE 1/q4.cpp
@@ -0,0 +1,18 @@
+/*Write a program to check whether a number is negative, zero, positive?*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    if (n<0){
+        cout<<n<<" is negative";
+    }
+    else if(n==0){
+        cout<<n<<" is zero";
+    }
+    else if(n>0){
+        cout<<n<<" is positive";
+    }
+
+}
