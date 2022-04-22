4523a1523ed3fd8131758181adb597af66f70f4b
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-04-23 01:05:39 +0530

    Add files via upload
    first four done

---
+++ MILESTONE 4/q1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,n,power;
+    cin>>x;
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        power=x*x;
+    }
+    cout<<power;
+}
---
+++ MILESTONE 4/q4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2,hcf;
+    cin>>n1;
+    cin>>n2;
+    int min;
+    if(n1>n2){
+        min=n2;
+    }
+    else{
+        min=n1;
+    }
+
+    for (int i=1;i<=min;i++){
+        if(n1%i==0 && n2%i==0){
+            hcf=i;
+        }
+    }
+    cout<<hcf;
+}
