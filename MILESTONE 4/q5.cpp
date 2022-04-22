d7959b705d15ab810e211ea23dcfa6ebc6134c78
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-04-23 01:50:01 +0530

    Add files via upload

---
+++ MILESTONE 4/q5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2,lcm,hcf;
+    cin>>n1;
+    cin>>n2;
+    int min;
+    if(n1>n2){
+        min=n2;
+    }
+    else{
+        min=n1;
+    }
+
+    for (int i=1;i<=min;i++){
+        if(n1%i==0 && n2%i==0){
+            hcf=i;
+        }
+    }
+    lcm=(n1*n2)/hcf;
+    cout<<lcm;
+
+}
