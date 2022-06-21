6ed4f88cfcfd00f8663d837f9dee1c0988e7ea93
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-06-21 08:15:04 +0530

    Update q5.cpp

--- MILESTONE 2/FIRST LINK/q5.cpp
+++ MILESTONE 2/FIRST LINK/q5.cpp
@@ -1,3 +1,12 @@
+/*A school has following rules for grading system:
+a. Below 25 - F
+b. 25 to 45 - E
+c. 45 to 50 - D
+d. 50 to 60 - C
+e. 60 to 80 - B
+f. Above 80 - A
+Ask user to enter marks and print the corresponding grade*/
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -28,4 +37,5 @@ int main(){
         cout<<"GRADE:A";
 
     }
+    return 0;
 }
