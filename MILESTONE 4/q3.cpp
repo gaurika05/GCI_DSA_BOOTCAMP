79908ac09934b674c512e03d3c64ff45779f0441
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-06-21 22:10:39 +0530

    Update q3.cpp

--- MILESTONE 4/q3.cpp
+++ MILESTONE 4/q3.cpp
@@ -1,11 +1,13 @@
+//Write a C program to calculate factorial of a number.
+
 #include<iostream>
 using namespace std;
 int main(){
     int i=1,n,fact=1;
-    i=1;
     cin>>n;
     for(;i<=n;i++){
         fact=fact*i;
     }
     cout<<fact;
+    return 0;
 }
