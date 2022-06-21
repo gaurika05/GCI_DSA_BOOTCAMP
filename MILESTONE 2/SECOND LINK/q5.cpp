ee8cbb9de2c9ccff5f722c2bc9880a55bd64a178
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-06-21 08:52:37 +0530

    Update q5.cpp

--- MILESTONE 2/SECOND LINK/q5.cpp
+++ MILESTONE 2/SECOND LINK/q5.cpp
@@ -1,3 +1,5 @@
+//Write a C program to print all odd number between 1 to 100.
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -8,5 +10,6 @@ int main(){
             }
             i++;
     }
+    return 0;
 
 }
