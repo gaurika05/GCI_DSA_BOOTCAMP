57520c0bf077c3f5ff58c5c337f9e64d8da54d3f
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-06-21 08:11:27 +0530

    Update q4.cpp

--- MILESTONE 2/FIRST LINK/q4.cpp
+++ MILESTONE 2/FIRST LINK/q4.cpp
@@ -1,3 +1,6 @@
+/*A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years.
+Ask user for their salary and year of service and print the net bonus amount.*/
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,4 +13,5 @@ int main(){
         double bonus=(5*salary)/100;
         cout<<"NET BONUS AMOUNT:"<<bonus;
     }
+    return 0;
 }
