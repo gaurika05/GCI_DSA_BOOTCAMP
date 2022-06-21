a3399a68769c7420c5bac3d44448a38a36756fc4
gaurika05 <97402240+gaurika05@users.noreply.github.com>
2022-06-21 08:49:52 +0530

    Update q4.cpp

--- MILESTONE 2/SECOND LINK/q4.cpp
+++ MILESTONE 2/SECOND LINK/q4.cpp
@@ -1,3 +1,5 @@
+//Write a C program to print all even numbers between 1 to 100. - using while loop
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -8,5 +10,5 @@ int main(){
             }
             i++;
     }
-
+    return 0;
 }
